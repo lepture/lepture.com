18c98af48483321b3ec5b814c9bb1e6ed0b42ebd
lepture <lepture@me.com>
2012-09-17 10:14:23 +0800

    update to version 3

--- settings.py
+++ settings.py
@@ -1,79 +1,87 @@
 # -*- coding: utf-8 -*-
 #: settings for liquidluck
 
-#: posts lay here
-source = 'content'
-
-#: generate html to ouput
-output = '_site'
-static_output = '_site/static'
-# static_prefix = '/static/'
-
-#: theme, load theme from _themes
-theme = 'moment'
-permalink = '{{category}}/{{filename}}/'
-timezone = '+08:00'
-
-
 #: site information
 site = {
     'name': 'Just lepture',
     'url': 'http://lepture.com',
     'feed': 'http://feeds.lepture.com/lepture',
-    'showtag': True,
 }
-author = 'lepture'
-authors = {
-    'lepture': {
-        'name': 'Hsiaoming Yang',
-        'website': 'http://lepture.com',
-        'email': 'lepture@me.com',
+
+config = {
+    'source': 'content',
+
+    #: generate html to ouput
+    'output': '_site',
+    'static': '_site/static',
+    #'static_prefix': '/static/',
+    'permalink': '{{category}}/{{filename}}/',
+    'relative_url': False,
+    'timezone': '+08:00',
+}
+
+
+author = {
+    'default': 'lepture',
+    'vars': {
+        'lepture': {
+            'name': 'Hsiaoming Yang',
+            'website': 'http://lepture.com',
+            'email': 'lepture@me.com',
+        }
     }
 }
 
 #: active readers
-readers = {
-    'rst': 'liquidluck.readers.restructuredtext.RestructuredTextReader',
+reader = {
+    'active': [
+        'liquidluck.readers.markdown.MarkdownReader',
+        'liquidluck.readers.restructuredtext.RestructuredTextReader',
+    ],
 }
 
 #: active writers
-writers = {
-    'tag': None,
-    'tagcloud': 'liquidluck.writers.core.TagCloudWriter'
-}
-
-writers_variables = {
-    'archive_output': 'archive.html',
-}
-
-#: template variables for rewrite themes variables
-template_variables = {
-}
-
-#: template filters
-template_filters = {
+writer = {
+    'active': [
+        'liquidluck.writers.core.PostWriter',
+        'liquidluck.writers.core.PageWriter',
+        'liquidluck.writers.core.ArchiveWriter',
+        'liquidluck.writers.core.ArchiveFeedWriter',
+        'liquidluck.writers.core.FileWriter',
+        'liquidluck.writers.core.StaticWriter',
+        'liquidluck.writers.core.YearWriter',
+        'liquidluck.writers.core.CategoryWriter',
+        'liquidluck.writers.core.CategoryFeedWriter',
+        #'liquidluck.writers.core.TagWriter',
+        'liquidluck.writers.core.TagCloudWriter',
+    ],
+    'vars': {
+        'archive_output': 'archive.html',
+    }
 }
 
 #: theme variables
-theme_variables = {
-    'disqus': 'lepture',
-    'analytics': 'UA-21475122-1',
+theme = {
+    'name': 'moment',
+    'vars': {
+        'disqus': 'lepture',
+        'analytics': 'UA-21475122-1',
+        'allow_comment_on_secret_post': True,
 
-    'allow_comment_on_secret_post': True,
+        'navigation': [
+            {'name': 'Blog', 'link': '/archive/'},
+            {'name': 'Life', 'link': '/life/'},
+            {'name': 'Work', 'link': '/work/'},
+            {'name': u'Résumé', 'link': '/resume/'},
+        ],
+        'elsewhere': [
+            {'name': 'GitHub', 'link': 'https://github.com/lepture'},
+            {'name': 'Twitter', 'link': 'https://twitter.com/lepture'},
+        ],
 
-    'navigation': [
-        ('Blog', '/archive/'),
-        ('Life', '/life/'),
-        ('Work', '/work/'),
-        (u'Résumé', '/resume/'),
-    ],
-    'elsewhere': [
-        ('GitHub', 'https://github.com/lepture'),
-        ('Twitter', 'https://twitter.com/lepture'),
-    ],
-
-    'descriptions': {
-        'life': u'生命是一襲華美的袍，爬滿了虱子 －－ 張愛玲',
-        'work': 'works in python, javascript, vim, and everything else'
-    },
+        'descriptions': {
+            'life': u'生命是一襲華美的袍，爬滿了虱子 －－ 張愛玲',
+            'work': 'works in python, javascript, vim, and everything else'
+        },
+    }
 }
