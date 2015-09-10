af44c8bc8414db06bd8bb85b9efb022a8c62ab11
Hsiaoming Yang <me@lepture.com>
2015-09-10 17:44:35 +0800

    Clean assets

---
+++ _includes/ga.js
@@ -0,0 +1,17 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', '{{ site.google_analytics }}');
+(function(d){
+  {% if page %}
+    var title="{{ page.title }}";
+  {% else %}
+    var title = document.title;
+  {% endif %}
+  ga('send', 'pageview', {
+    page: location.pathname,
+    location: location.href,
+    title: title,
+  });
+})(document);
